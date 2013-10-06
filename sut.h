e3cd73f70ffe79a325e785eb4bde6f8b8658b36e
Tor Kreutzer <tor.kreutzer@gmail.com>
2013-10-06 23:38:00 +0200

    names simple, not pico...

--- sut.h
+++ sut.h
@@ -4,23 +4,23 @@
 #define DESCRIBE(system_under_test) \
 auto main() -> int \
 { \
-    int pico_failed_tests = 0; \
-    const char * pico_current_test = nullptr; \
+    int simple_failed_tests = 0; \
+    const char * simple_current_test = nullptr; \
     fprintf(stdout, "\n%s\n  ", system_under_test); \
 
 #define END() \
-    fprintf(stdout, "\n\n%d failed tests\n\n", pico_failed_tests); \
-    return pico_failed_tests; \
+    fprintf(stdout, "\n\n%d failed tests\n\n", simple_failed_tests); \
+    return simple_failed_tests; \
 }
 
-#define IT(pico_test_name, expr) \
-    pico_current_test = pico_test_name; \
+#define IT(simple_test_name, expr) \
+    simple_current_test = simple_test_name; \
     try { [&](){ expr }(); fprintf(stdout, "."); } \
-    catch (...) { pico_failed_tests++; fprintf(stdout, "f"); }
+    catch (...) { simple_failed_tests++; fprintf(stdout, "f"); }
 
 #define S_ASSERT(condition, msg) \
 if( !(condition) ) { \
-    fprintf(stderr, "\n  %s.\n  It %s! It\'s not!\n", msg, pico_current_test); \
+    fprintf(stderr, "\n  %s.\n  It %s! It\'s not!\n", msg, simple_current_test); \
     fprintf(stderr, "  Assertion: %s\n", #condition); \
     fprintf(stderr, "    at %s:%d\n\n\t", __FILE__, __LINE__); \
     throw 1; \
